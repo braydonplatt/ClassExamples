2362056838f295f1a11258c7363c0f31f0dd2b6c
Braydon Platt <braydonplatt@gmail.com>
2015-01-21 16:07:33 -0700

    Working on Problems

--- classExamples.js
+++ classExamples.js
@@ -1,1 +1,74 @@
-fdjkal;fjdskal;jfkdla;
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem 
+
+  //code here for sayHi
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+   
+
+and what you should write is the favNum function that makes the code above work, 
+    
+    
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+    
+    
+*/
+var first = function(arr, cb){
+  cb(names[0]);
+}
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ', firstName)
+});
+
+//next problem
+var last = (arr, cb){
+  cb(names[names.length-1]);
+}
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+
+//next problem
+
+var multiply = function(num1, num2, callback){
+  var nums = num1 * num2;
+      callback(nums);
+}
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ', answer); //should console.log 12
+})
+
+
+//next problem
+
+ var contains = function(arr, str, callback){
+   for (var i = 0; i <arr.length; i++){
+    if (str === arr[i]){
+      callback(true);
+    }
+    }
+   };
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+contains(names, 'Colt', function(result){
+  if(result === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
