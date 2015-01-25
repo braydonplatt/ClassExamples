42d3f8b8e540f383c55a682ea70bd988b71541cc
Braydon Platt <braydonplatt@gmail.com>
2015-01-25 00:50:27 -0700

    getting tired its late

--- classExamples.js
+++ classExamples.js
@@ -467,10 +467,106 @@ CallBACks
 
 
 var first = function(arr, callback){
-  callback names[0];
+  callback(names[0]);
 }
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
+});
+
+
+
+var last = function(arr, callback){
+  callback(arr[arr.length - 1])
+}
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+
+
+
+
+var multiply = function(num1, num2, callback) {
+  var nums = num1 * num2;
+  callback(nums);
+};
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ', answer); //should console.log 12
+})
+
+
+var uniq = function(arr, callback){
+  var newArr = [];
+    for(var i = 0; i < arr.length; i++){
+      if newArr.indexOf(arr[i]) < 0){
+        newArr.push(arr[i]);
+        callback(newArr);
+      }  
+} 
+    }
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+
+
+var names = function(arr, callback){
+  for (var i = 0; i < arr.length; i++){
+    callback(arr[i], i)
+  }
+};
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+
+
+
+
+
+var getUserById = function(arr, str, callback){
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i].id === str);
+    callback(arr[i]);
+
+
+ //code here for getUserById
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
