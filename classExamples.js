b1c6c74b423f974eedcc78efb7418a95a9cf2080
Braydon Platt <braydonplatt@gmail.com>
2015-01-26 16:20:55 -0700

    Working on function

--- classExamples.js
+++ classExamples.js
@@ -834,10 +834,73 @@ alert("The total number was " + result);
 
 
 
+//Once you complete a problem, open up Chrome and check the answer in the console.
+
+
+var name = 'Tyler';
+//Create a function called isTyler that accepts name as it's only argument.
+//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+
+var isTyler = function(myName){
+  if (myName === "Tyler") {
+    return true;
+  } else {
+    return false;
+  }
+};
+isTyler(name);
 
 
 
 
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
+var getName = function(){
+  var user = prompt('What is your name?');
+  return user;
+}
+
+
+
+//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+//then alerts "Welcome, " plus whatever the users name is.
+
+
+var welcome = function(){
+  var name = getName;
+  alert("Welcome, " + name);
+};
+welcome();
+
+
+
+//Write a function called adder that uses the 'arguments' keyword that comes with every function to
+//loop over all the arguments the function received, add them all together, then return the result.
+//Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
+//returned from adder.
+
+
+var adder = function(num1, num2, num3){
+  var total = 0;
+  for (var i = 0; i < arguments.length; i++){
+    total = total + 1;
+    return total;
+  };
+};
+var result = adder(1,4,6);
+alert("The total number was " + result);
+
+
+
+
+//Create a function called outerFn which returns an anonymous function which returns your name.
+
+
+
+var outerFn = function(){
+  return function(){
+    return "Braydon";
+  }
+};
 
