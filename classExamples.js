914709875e796c81b72245a670c1d992a88cfe13
Braydon Platt <braydonplatt@gmail.com>
2015-02-09 11:13:18 -0700

    worked on objects

--- classExamples.js
+++ classExamples.js
@@ -1414,3 +1414,108 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 
+
+
+
+
+
+
+
+
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+
+ var favoriteThings = {
+    band: 'Coldplay',
+    food: 'Tacos',
+    person: 'Mikki',
+    book: 'Linchpin',
+    movie: 'Bourne',
+    holiday: 'Christmas'
+ }
+
+//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+
+favoriteThings.car = 'Porsche';
+favoriteThings.brand = 'Apple';
+
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+
+favoriteThings.food = 'lettuce';
+favoriteThings.book = '50 shades';
+
+//Now, alert your favorite person, then alert your favorite book.
+
+alert(favoriteThings.person);
+alert(favoriteThings.book);
+
+
+
+/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+
+
+
+var user = {
+    name: 'Tyler McGinnis', 
+    email: null,
+    pwHash: 'U+Ldlngx2BYQk',
+    birthday: undefined,
+    username: 'tylermcginnis33',
+    age: 0
+}
+
+//Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
+
+ for (var key in user){
+   if(!user[key])
+    delete user[key];
+ }
+
+ console.log(user);
+
+//Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
+
+user.name = 'Braydon Platt';
+user.pwHash = 'brrDawg';
+user.username = 'braydon32';
+
+//Now console.log your object and make sure it looks right.
+
+console.log(user);
+
+
+
+/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+
+
+
+//create an empty object called methodCollection. 
+
+ methodCollection = {};
+
+//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
+
+var alertHello = function(){
+  alert('hello world')
+};
+
+var consoleHello = function(){
+  console.log('hello world')
+};
+
+//Now call your alertHello and logHello methods. 
+
+consoleHello();
+alertHello();
+
+
+
+/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+
+
+
+//write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
+//voweler("This is a test") --> {i: 2, a: 1, e: 1};
+
+
+
+
